0f571485b18e412ea4e65ca7d0a8c0cbff1c1190
Shaman-Laptop <lilki@DESKTOP-GBC3VNF>
2023-06-13 13:32:00 -0500

    Fixed finite Supply Error and rename SANDY token name to STST

--- SANDY-MAIN/Output/limb.asm
+++ SANDY-MAIN/Output/limb.asm
@@ -23,36 +23,37 @@
 // Line 23:    skinType: string;
 // Line 24:
 // Line 25:}
-// Line 26:token SANDY {
-// Line 27:    import Array;
-// Line 28:    import NFT;
-// Line 29:    import Runtime;
-// Line 30:    import Time;
-// Line 31:    import Token;
-// Line 32:    import Map;
-// Line 33:
-// Line 34:    global owner: address;
-// Line 35:    global owns_sandy: storage_map<address,bool>; 
-// Line 36:    global _infusionAmt: number;
-// Line 37:    /*
-// Line 38:     - Can use the seriesID as the number and can use randomization that way. Will have to think more about this
-// Line 39:    */
-// Line 40:    global _limbs: storage_map<string,number>; //wanted this to be an array so I can get index by modulus. Will test out.
-// Line 41:    global _categories: storage_map<string,number>;
-// Line 42:    global _infusionTkn: string;
-// Line 43:
+// Line 26://Will change once complete
+// Line 27:token STST {
+// Line 28:    import Array;
+// Line 29:    import NFT;
+// Line 30:    import Runtime;
+// Line 31:    import Time;
+// Line 32:    import Token;
+// Line 33:    import Map;
+// Line 34:
+// Line 35:    global owner: address;
+// Line 36:    global owns_sandy: storage_map<address,bool>; 
+// Line 37:    global _infusionAmt: number;
+// Line 38:    /*
+// Line 39:     - Can use the seriesID as the number and can use randomization that way. Will have to think more about this
+// Line 40:    */
+// Line 41:    global _limbs: storage_map<string,number>; //wanted this to be an array so I can get index by modulus. Will test out.
+// Line 42:    global _categories: storage_map<string,number>;
+// Line 43:    global _infusionTkn: string;
 // Line 44:
-// Line 45:	const DOLL_SUPPLY: number = 500;
-// Line 46:    const SANDY_SERIESID: number = 1;
-// Line 47:    property name: string = "Test";//Will change to SANDY once thoroughly tested
-// Line 48:    property symbol: string = "TST";// Will change once properly tested
-// Line 49:    property isFungible: bool = false;
-// Line 50:    property isFinite: bool = true;
-// Line 51:    property isBurnable: bool = true;
-// Line 52:	property maxSupply: number = DOLL_SUPPLY;
-// Line 53:    property owner: address = owner;
-// Line 54:    
-// Line 55:    nft limb<limb_rom,limb_ram> {
+// Line 45:
+// Line 46:	const DOLL_SUPPLY: number = 500;
+// Line 47:    const SANDY_SERIESID: number = 1;
+// Line 48:    property name: string = "Test";//Will change to SANDY once thoroughly tested
+// Line 49:    property symbol: string = "STST";// Will change once properly tested
+// Line 50:    property isFungible: bool = false;
+// Line 51:    property isFinite: bool = true;
+// Line 52:    property isBurnable: bool = true;
+// Line 53:	property maxSupply: number = 70000;//random test value, Might want maxSupply set to 0 and isFinite set to false as well
+// Line 54:    property owner: address = owner;
+// Line 55:    
+// Line 56:    nft limb<limb_rom,limb_ram> {
 
 // ********* getName Property ***********
 @entry_getName: // 0
@@ -61,111 +62,111 @@ POP r1 // get nft tokenID from stack // 1
 LOAD r0 "ROM" // 3
 PUSH r0 // fields // 10
 PUSH r1 // tokenID // 12
-LOAD r0 "SANDY" // 14
-PUSH r0 // symbol // 23
-LOAD r0 "Runtime.ReadToken" // 25
-EXTCALL r0 // 46
-POP r2 // 48
-UNPACK r2 r2 // 50
-ALIAS r3 $_ROM // 53
-LOAD r0 "ROM" // 53
-GET r2 $_ROM r0 // 60
-UNPACK $_ROM $_ROM // 64
-// Line 56:
-// Line 57:    property name: string{
-// Line 58:        return _ROM.name;
-	COPY $_ROM r1 // 67
-	LOAD r2 "name" // 70
-	GET r1 r1 r2 // 78
-	PUSH r1 // 82
-	JMP @exit_getName // 84
-@exit_getName: // 87
-RET // 88
-// Line 59:
-// Line 60:    }
+LOAD r0 "STST" // 14
+PUSH r0 // symbol // 22
+LOAD r0 "Runtime.ReadToken" // 24
+EXTCALL r0 // 45
+POP r2 // 47
+UNPACK r2 r2 // 49
+ALIAS r3 $_ROM // 52
+LOAD r0 "ROM" // 52
+GET r2 $_ROM r0 // 59
+UNPACK $_ROM $_ROM // 63
+// Line 57:
+// Line 58:    property name: string{
+// Line 59:        return _ROM.name;
+	COPY $_ROM r1 // 66
+	LOAD r2 "name" // 69
+	GET r1 r1 r2 // 77
+	PUSH r1 // 81
+	JMP @exit_getName // 83
+@exit_getName: // 86
+RET // 87
+// Line 60:
+// Line 61:    }
 
 // ********* getDescription Property ***********
-@entry_getDescription: // 89
-POP r1 // get nft tokenID from stack // 90
+@entry_getDescription: // 88
+POP r1 // get nft tokenID from stack // 89
 // reading nft data
-LOAD r0 "ROM" // 92
-PUSH r0 // fields // 99
-PUSH r1 // tokenID // 101
-LOAD r0 "SANDY" // 103
-PUSH r0 // symbol // 112
-LOAD r0 "Runtime.ReadToken" // 114
-EXTCALL r0 // 135
-POP r2 // 137
-UNPACK r2 r2 // 139
-ALIAS r3 $_ROM // 142
-LOAD r0 "ROM" // 142
-GET r2 $_ROM r0 // 149
-UNPACK $_ROM $_ROM // 153
-// Line 61:
-// Line 62:    property description: string{
-// Line 63:        return _ROM.description;
-	COPY $_ROM r1 // 156
-	LOAD r2 "description" // 159
-	GET r1 r1 r2 // 174
-	PUSH r1 // 178
-	JMP @exit_getDescription // 180
-@exit_getDescription: // 183
-RET // 184
-// Line 64:
-// Line 65:    }
+LOAD r0 "ROM" // 91
+PUSH r0 // fields // 98
+PUSH r1 // tokenID // 100
+LOAD r0 "STST" // 102
+PUSH r0 // symbol // 110
+LOAD r0 "Runtime.ReadToken" // 112
+EXTCALL r0 // 133
+POP r2 // 135
+UNPACK r2 r2 // 137
+ALIAS r3 $_ROM // 140
+LOAD r0 "ROM" // 140
+GET r2 $_ROM r0 // 147
+UNPACK $_ROM $_ROM // 151
+// Line 62:
+// Line 63:    property description: string{
+// Line 64:        return _ROM.description;
+	COPY $_ROM r1 // 154
+	LOAD r2 "description" // 157
+	GET r1 r1 r2 // 172
+	PUSH r1 // 176
+	JMP @exit_getDescription // 178
+@exit_getDescription: // 181
+RET // 182
+// Line 65:
+// Line 66:    }
 
 // ********* getInfoURL Property ***********
-@entry_getInfoURL: // 185
-POP r1 // get nft tokenID from stack // 186
+@entry_getInfoURL: // 183
+POP r1 // get nft tokenID from stack // 184
 // reading nft data
-LOAD r0 "ROM" // 188
-PUSH r0 // fields // 195
-PUSH r1 // tokenID // 197
-LOAD r0 "SANDY" // 199
-PUSH r0 // symbol // 208
-LOAD r0 "Runtime.ReadToken" // 210
-EXTCALL r0 // 231
-POP r2 // 233
-UNPACK r2 r2 // 235
-ALIAS r3 $_ROM // 238
-LOAD r0 "ROM" // 238
-GET r2 $_ROM r0 // 245
-UNPACK $_ROM $_ROM // 249
-// Line 66:    property infoURL: string{
-// Line 67:        return _ROM.infoURL;
-	COPY $_ROM r1 // 252
-	LOAD r2 "infoURL" // 255
-	GET r1 r1 r2 // 266
-	PUSH r1 // 270
-	JMP @exit_getInfoURL // 272
-@exit_getInfoURL: // 275
-RET // 276
-// Line 68:
-// Line 69:    }
+LOAD r0 "ROM" // 186
+PUSH r0 // fields // 193
+PUSH r1 // tokenID // 195
+LOAD r0 "STST" // 197
+PUSH r0 // symbol // 205
+LOAD r0 "Runtime.ReadToken" // 207
+EXTCALL r0 // 228
+POP r2 // 230
+UNPACK r2 r2 // 232
+ALIAS r3 $_ROM // 235
+LOAD r0 "ROM" // 235
+GET r2 $_ROM r0 // 242
+UNPACK $_ROM $_ROM // 246
+// Line 67:    property infoURL: string{
+// Line 68:        return _ROM.infoURL;
+	COPY $_ROM r1 // 249
+	LOAD r2 "infoURL" // 252
+	GET r1 r1 r2 // 263
+	PUSH r1 // 267
+	JMP @exit_getInfoURL // 269
+@exit_getInfoURL: // 272
+RET // 273
+// Line 69:
+// Line 70:    }
 
 // ********* getImageURL Property ***********
-@entry_getImageURL: // 277
-POP r1 // get nft tokenID from stack // 278
+@entry_getImageURL: // 274
+POP r1 // get nft tokenID from stack // 275
 // reading nft data
-LOAD r0 "ROM" // 280
-PUSH r0 // fields // 287
-PUSH r1 // tokenID // 289
-LOAD r0 "SANDY" // 291
-PUSH r0 // symbol // 300
-LOAD r0 "Runtime.ReadToken" // 302
-EXTCALL r0 // 323
-POP r2 // 325
-UNPACK r2 r2 // 327
-ALIAS r3 $_ROM // 330
-LOAD r0 "ROM" // 330
-GET r2 $_ROM r0 // 337
-UNPACK $_ROM $_ROM // 341
-// Line 70:    property imageURL: string{
-// Line 71:        return _ROM.imageURL;
-	COPY $_ROM r1 // 344
-	LOAD r2 "imageURL" // 347
-	GET r1 r1 r2 // 359
-	PUSH r1 // 363
-	JMP @exit_getImageURL // 365
-@exit_getImageURL: // 368
-RET // 369
+LOAD r0 "ROM" // 277
+PUSH r0 // fields // 284
+PUSH r1 // tokenID // 286
+LOAD r0 "STST" // 288
+PUSH r0 // symbol // 296
+LOAD r0 "Runtime.ReadToken" // 298
+EXTCALL r0 // 319
+POP r2 // 321
+UNPACK r2 r2 // 323
+ALIAS r3 $_ROM // 326
+LOAD r0 "ROM" // 326
+GET r2 $_ROM r0 // 333
+UNPACK $_ROM $_ROM // 337
+// Line 71:    property imageURL: string{
+// Line 72:        return _ROM.imageURL;
+	COPY $_ROM r1 // 340
+	LOAD r2 "imageURL" // 343
+	GET r1 r1 r2 // 355
+	PUSH r1 // 359
+	JMP @exit_getImageURL // 361
+@exit_getImageURL: // 364
+RET // 365
